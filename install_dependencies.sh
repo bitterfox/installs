199b4b633e99f89721787a8e9a7e10ff8f895900
yoshida.shinya <yoshida.shinya@linecorp.com>
2018-07-18 10:52:30 +0900

    Add jq

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -5,8 +5,8 @@ if [ "--fast" != "$1" ]; then
 fi
 
 # basic components
-sudo apt install git mercurial subversion emacs zsh xclip curl fcitx-mozc
-sudo apt install openjdk-8-jdk maven
+sudo apt install git mercurial subversion emacs zsh xclip curl fcitx-mozc jq
+sudo apt install openjdk-8-jdk openjdk-8-source maven
 
 if [ "--fast" != "$1" ]; then
     sudo apt update
