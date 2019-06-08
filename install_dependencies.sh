5ede55187f90faeb74580c5065df929e9374648a
yoshida.shinya <yoshida.shinya@linecorp.com>
2019-06-08 19:42:28 +0900

    Update

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -5,8 +5,8 @@ if [ "--fast" != "$1" ]; then
 fi
 
 # basic components
-sudo apt install git mercurial subversion emacs zsh xclip curl fcitx-mozc jq
-sudo apt install openjdk-8-jdk openjdk-8-source maven
+sudo apt install git mercurial subversion emacs zsh xclip curl fcitx-mozc jq gdebi
+sudo apt install openjdk-8-jdk openjdk-8-source maven autoconf libx11-dev libxext-dev libxrender-dev libxtst-dev libxt-dev libcups2-dev libasound2-dev ant
 
 if [ "--fast" != "$1" ]; then
     sudo apt update
