c739650d73b084e7b1c20d7458fc96cb126dd684
bitter_fox <bitterfoxc@gmail.com>
2020-05-20 13:38:38 +0900

    Fix setup xkeysnail

--- install_xkeysnail.sh
+++ install_xkeysnail.sh
@@ -1,3 +1,7 @@
 
 sudo apt install python3-pip
 sudo pip3 install xkeysnail
+
+sudo cp sudoers_xkeysnail /etc/sudoers.d
+sudo chown root:root /etc/sudoers.d/sudoers_xkeysnail
+sudo chmod 440 /etc/sudoers.d/sudoers_xkeysnail
