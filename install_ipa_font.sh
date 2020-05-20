5ad33eba1fb516a68cafa0bc9512d19ea8e1b3e8
yoshida.shinya <yoshida.shinya@linecorp.com>
2020-05-20 17:43:43 +0900

    Fix install

--- install_ipa_font.sh
+++ install_ipa_font.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 sudo mkdir -p /usr/share/fonts/truetype/ttf-ipa-font
-wget -O /tmp/ipafont.zip https://ipafont.ipa.go.jp/IPAexfont/IPAexfont00401.zip
+wget -O /tmp/ipafont.zip https://ipafont.ipa.go.jp/IPAfont/IPAfont00303.zip
 unzip /tmp/ipafont.zip -d /tmp
 
-sudo cp /tmp/IPAexfont00401/*.ttf /usr/share/fonts/truetype/ttf-ipa-font
+sudo cp /tmp/IPAfont00303/*.ttf /usr/share/fonts/truetype/ttf-ipa-font
 
 fc-cache -fv
--- install_slack.sh
+++ install_slack.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-wget -O /tmp/slack.deb https://downloads.slack-edge.com/linux_releases/slack-desktop-3.4.2-amd64.deb
+wget -O /tmp/slack.deb https://downloads.slack-edge.com/linux_releases/slack-desktop-4.4.3-amd64.deb
 sudo gdebi /tmp/slack.deb
