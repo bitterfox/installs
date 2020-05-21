22d0da776a6db7bf9993857ffc75c4a19a8ce0e3
yoshida.shinya <yoshida.shinya@linecorp.com>
2020-05-21 15:56:27 +0900

    Restore preset

--- install_pulseaudio_equalizer.sh
+++ install_pulseaudio_equalizer.sh
@@ -8,3 +8,6 @@ pactl load-module module-dbus-protocol
 sudo sh -c 'echo "### Load the integrated pulseaudio equalizer and dbus module" >> /etc/pulse/default.pa'
 sudo sh -c 'echo "load-module module-equalizer-sink" >> /etc/pulse/default.pa'
 sudo sh -c 'echo "load-module module-dbus-protocol" >> /etc/pulse/default.pa'
+
+mkdir -p ~/.config/pulse
+cp equalizer-presets.tdb ~/.config/pulse
