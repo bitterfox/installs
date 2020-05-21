a9c16e6a173c2d088751776269e1eefa1248eadc
yoshida.shinya <yoshida.shinya@linecorp.com>
2020-05-21 12:01:34 +0900

    Fix

--- install_xkeysnail.sh
+++ install_xkeysnail.sh
@@ -1,6 +1,7 @@
 
 sudo apt install python3-pip
-sudo pip3 install xkeysnail
+# xkeysnail v0.3.0 has an issue with key combination like C-x+C-x
+sudo pip3 install xkeysnail==0.2.0
 
 sudo cp sudoers_xkeysnail /etc/sudoers.d
 sudo chown root:root /etc/sudoers.d/sudoers_xkeysnail
--- setup_desktop.sh
+++ setup_desktop.sh
@@ -1,1 +1,2 @@
 gsettings set org.gnome.nautilus.desktop trash-icon-visible false
+gsettings set org.gnome.nautilus.desktop home-icon-visible false
