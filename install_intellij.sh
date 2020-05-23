652405962a836de151827deefdd7e2fd97f41194
yoshida.shinya <yoshida.shinya@linecorp.com>
2020-05-24 00:35:09 +0900

    Use symlink for idea

--- install_intellij.sh
+++ install_intellij.sh
@@ -5,4 +5,4 @@ mkdir -p ~/bin/
 wget -O /tmp/idea.tar.gz "https://data.services.jetbrains.com/products/download?code=IIU&platform=linux"
 tar xvfz /tmp/idea.tar.gz -C ~/bin/
 
-cp ~/bin/idea*/bin/idea.sh ~/bin/idea
+ln -s ~/bin/idea*/bin/idea.sh ~/bin/idea
