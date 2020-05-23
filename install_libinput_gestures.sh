8d98e2df6916cadd97a054657f33c235aa8ed97a
yoshida.shinya <yoshida.shinya@linecorp.com>
2020-05-23 21:13:56 +0900

    Setup sudoer

--- install_libinput_gestures.sh
+++ install_libinput_gestures.sh
@@ -8,3 +8,7 @@ fi
 git clone --depth 1 https://github.com/bulletmark/libinput-gestures.git ~/.libinput-gestures
 cd ~/.libinput-gestures/install
 sudo make install
+
+sudo cp sudoers_libinput_gestures /etc/sudoers.d
+sudo chown root:root /etc/sudoers.d/sudoers_libinput_gestures
+sudo chmod 440 /etc/sudoers.d/sudoers_libinput_gestures
