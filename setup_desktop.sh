d047f362b3489f61c5446f258957aaecf864c462
yoshida.shinya <yoshida.shinya@linecorp.com>
2020-05-24 00:24:26 +0900

    Add set background image

--- setup_desktop.sh
+++ setup_desktop.sh
@@ -1,2 +1,3 @@
 gsettings set org.gnome.nautilus.desktop trash-icon-visible false
 gsettings set org.gnome.nautilus.desktop home-icon-visible false
+gsettings set org.gnome.desktop.background picture-uri "file:///home/bitterfox/Pictures/Lza1qCX.png"
