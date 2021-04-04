c65590124036239cbeb07176a4c167feb8e3e083
yoshida.shinya <yoshida.shinya@linecorp.com>
2021-04-04 13:28:11 +0900

    Add pulseeffects

---
+++ install_pulseeffects.sh
@@ -0,0 +1,6 @@
+
+# Ubuntu 18.04
+# https://github.com/wwmm/pulseeffects/wiki/Package-Repositories#debian--ubuntu
+sudo add-apt-repository ppa:mikhailnov/pulseeffects -y
+sudo apt update
+sudo apt install pulseeffects pulseaudio --install-recommends
