e619a9b066643cb1a00c361867f05d9f0001873a
yoshida.shinya <yoshida.shinya@linecorp.com>
2021-04-04 15:37:04 +0900

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,17 @@
+# System
+./setup_desktop.sh
+./setup_english_home_directory.sh
+./setup_fcitx.sh
+./setup_swap.sh
+
+# Shell
+./setup_chsh.sh
+./setup_gnome_terminal.sh
+
+# Inputs: Keyboard and trackpad
+./setup_ctrl_caps.sh
+./setup_fnmode.sh
+./setup_keyboard_repeat.sh
+./setup_libinput_quirks.sh
+
+./setup_xps13_fan.sh
--- setup_xps13_fan.sh
+++ setup_xps13_fan.sh
@@ -1,4 +1,7 @@
 # https://askubuntu.com/questions/1094485/dell-xps-15-9570-how-to-control-the-fans
-sudo sh -c 'echo "options dell-smm-hwmon restricted=0 force=1" > /etc/modprobe.d/dell-smm-hwmon.conf'
-sudo sh -c 'echo "dell-smm-hwmon" > /etc/modules'
-sudo update-initramfs -u
+if [ "`sudo dmidecode -s system-product-name`" = "XPS 13 9300" ]; then
+    echo "XPS 13 9300: Setup fan"
+    sudo sh -c 'echo "options dell-smm-hwmon restricted=0 force=1" > /etc/modprobe.d/dell-smm-hwmon.conf'
+    sudo sh -c 'echo "dell-smm-hwmon" > /etc/modules'
+    sudo update-initramfs -u
+fi
