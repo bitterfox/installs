44c85076d87d5a49e2ed343769d14205a224dd61
yoshida.shinya <yoshida.shinya@linecorp.com>
2021-04-04 13:47:52 +0900

    Add gimp

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -6,7 +6,7 @@ fi
 
 # basic components
 sudo apt install git mercurial subversion emacs zsh xclip curl fcitx-mozc jq gdebi
-sudo apt install openjdk-8-jdk openjdk-8-source maven autoconf libx11-dev libxext-dev libxrender-dev libxtst-dev libxt-dev libcups2-dev libasound2-dev ant net-tools
+sudo apt install openjdk-8-jdk openjdk-8-source maven autoconf libx11-dev libxext-dev libxrender-dev libxtst-dev libxt-dev libcups2-dev libasound2-dev ant net-tools gimp
 
 if [ "--fast" != "$1" ]; then
     sudo apt update
