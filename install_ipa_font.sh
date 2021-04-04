52da74215985cb0da877bfdd05704c607d2d4954
yoshida.shinya <yoshida.shinya@linecorp.com>
2021-04-04 19:40:49 +0900

    Update url for ipa font

--- install_ipa_font.sh
+++ install_ipa_font.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo mkdir -p /usr/share/fonts/truetype/ttf-ipa-font
-wget -O /tmp/ipafont.zip https://ipafont.ipa.go.jp/IPAfont/IPAfont00303.zip
+wget -O /tmp/ipafont.zip https://moji.or.jp/wp-content/ipafont/IPAfont/IPAfont00303.zip
 unzip /tmp/ipafont.zip -d /tmp
 
 sudo cp /tmp/IPAfont00303/*.ttf /usr/share/fonts/truetype/ttf-ipa-font
