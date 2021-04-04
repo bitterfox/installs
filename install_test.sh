a6886129965a5f93ee5a4a77c1646c5ced3ae0f5
yoshida.shinya <yoshida.shinya@linecorp.com>
2021-04-04 16:17:49 +0900

    Improve install

--- install_test.sh
+++ install_test.sh
@@ -1,18 +1,47 @@
 #!/bin/bash
 
 install() {
-    path=$1
+    item=$1
+
+    if [ "$INSTALL_ALL" != "true" ]; then
+        choose_install "$item"
+        if [ $? -eq 0 ]; then
+            echo "Skip installing $item"
+            return
+        fi
+    fi
+
     cat <<EOF
 ####################################################################################################
 #                                          Install $path
 ####################################################################################################
 EOF
-    ./install_${path}.sh
+    ./install_${item}.sh
     cat <<EOF
 ####################################################################################################
 #                                          Done $path
 ####################################################################################################
 EOF
 }
+
+choose_interactive() {
+    echo -n "Install one by one (Y/n) >"
+    read out
+    if [ "$out" = "n" ]; then
+        export INSTALL_ALL="true"
+    fi
+}
+
+choose_install() {
+    item=$1
+    echo -n "Install $item (Y/n) >"
+    read out
+    if [ "$out" = "n" ]; then
+        return 0
+    fi
+    return 1
+}
+
+choose_interactive
 
 install dummy
