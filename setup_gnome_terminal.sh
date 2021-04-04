d65676b858e8f88a07d99a52cb1ec58dea38b0ff
yoshida.shinya <yoshida.shinya@linecorp.com>
2021-04-04 13:30:15 +0900

    Ubuntu 20.04 gnome-terminal

--- setup_gnome_terminal.sh
+++ setup_gnome_terminal.sh
@@ -3,4 +3,9 @@
 gsettings set org.gnome.Terminal.Legacy.Settings confirm-close false
 gsettings set org.gnome.Terminal.Legacy.Settings default-show-menubar false
 
+if [ -n "`gsettings list-keys org.gnome.Terminal.Legacy.Settings | grep 'headerbar'`" ]; then
+    echo "Disable header bar"
+    gsettings set org.gnome.Terminal.Legacy.Settings headerbar false
+fi
+
 dconf load /org/gnome/terminal/legacy/profiles:/ < gnome_profiles
