315701f275f31c7aecc9156e8013c18200b5f3af
yoshida.shinya <yoshida.shinya@linecorp.com>
2021-04-04 17:37:30 +0900

    Intractive setup

--- setup.sh
+++ setup.sh
@@ -1,17 +1,63 @@
+#!/bin/bash
+
+setup() {
+    item=$1
+
+    if [ "$SETUP_ALL" != "true" ]; then
+        choose_setup "$item"
+        if [ $? -eq 0 ]; then
+            echo "Skip setup $item"
+            return
+        fi
+    fi
+
+    cat <<EOF
+####################################################################################################
+#                                          Setup $item
+####################################################################################################
+EOF
+    ./setup_${item}.sh
+    cat <<EOF
+####################################################################################################
+#                                          Done $item
+####################################################################################################
+EOF
+}
+
+choose_interactive() {
+    echo -n "Setup one by one (Y/n) >"
+    read out
+    if [ "$out" = "n" ]; then
+        export SETUP_ALL="true"
+    fi
+}
+
+choose_setup() {
+    item=$1
+    echo -n "Setup $item (Y/n) >"
+    read out
+    if [ "$out" = "n" ]; then
+        return 0
+    fi
+    return 1
+}
+
+choose_interactive
+
 # System
-./setup_desktop.sh
-./setup_english_home_directory.sh
-./setup_fcitx.sh
-./setup_swap.sh
+setup desktop
+setup english_home_directory
+setup fcitx
+setup swap
 
 # Shell
-./setup_chsh.sh
-./setup_gnome_terminal.sh
+setup chsh
+setup gnome_terminal
 
 # Inputs: Keyboard and trackpad
-./setup_ctrl_caps.sh
-./setup_fnmode.sh
-./setup_keyboard_repeat.sh
-./setup_libinput_quirks.sh
+setup ctrl_caps
+setup fnmode
+setup keyboard_repeat
+setup libinput_quirks
 
-./setup_xps13_fan.sh
+setup xps13_fan
