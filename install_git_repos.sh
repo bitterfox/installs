28813cbcc2bac5d40e368fc0a688c7ec4985f83f
yoshida.shinya <yoshida.shinya@linecorp.com>
2021-07-04 15:24:49 +0900

    Update git-repos

--- install_git_repos.sh
+++ install_git_repos.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
+echo "Bootstrap git-repos"
 git clone https://github.com/bitterfox/gitrepos.git /tmp/gitrepos
-/tmp/gitrepos/git-repos clone git@github.com:bitterfox/gitrepos.git
+
+echo "Initialize git-repos"
+/tmp/gitrepos/git-repos clone https://github.com/bitterfox/gitrepos.git
+
+echo "Rewrite remote to SSH"
+git -C "`/tmp/gitrepos/git-repos root`/github.com/bitterfox/gitrepos" remote -v
+git -C "`/tmp/gitrepos/git-repos root`/github.com/bitterfox/gitrepos" remote set-url origin git@github.com:bitterfox/gitrepos.git
+git -C "`/tmp/gitrepos/git-repos root`/github.com/bitterfox/gitrepos" remote -v
