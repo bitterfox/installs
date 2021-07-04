3a6b6a60dfc001517b39245d3256f263adc01436
yoshida.shinya <yoshida.shinya@linecorp.com>
2021-07-04 15:44:48 +0900

    Fix spec/line

--- spec/line.sh
+++ spec/line.sh
@@ -2,4 +2,4 @@
 
 . "`dirname $0`/spec.sh"
 
-exists_file "$HOME/.wine/drive_c/users/bitterfox/Local Settings/Application Data/LINE/bin/current/LINE.exe"
+exists_file "$HOME/.wine/drive_c/users/$USER/Local Settings/Application Data/LINE/bin/current/LINE.exe"
