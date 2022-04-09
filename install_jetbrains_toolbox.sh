518c6bb1a7548f4b80992280b701a157ff634751
yoshida.shinya <yoshida.shinya@linecorp.com>
2022-04-09 17:27:26 +0900

    Update jetbrains toolbox

--- install_jetbrains_toolbox.sh
+++ install_jetbrains_toolbox.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-wget -O /tmp/tookbox.tar.gz "https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.14.5179.tar.gz"
+wget -O /tmp/tookbox.tar.gz "https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.23.11731.tar.gz"
 tar xvfz /tmp/tookbox.tar.gz -C ~/bin
