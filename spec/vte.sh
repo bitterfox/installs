768d972cc1095ccf97a45d2bc9cf4beb38e3313c
yoshida.shinya <yoshida.shinya@linecorp.com>
2022-04-09 19:17:39 +0900

    Fix vte spec

--- spec/vte.sh
+++ spec/vte.sh
@@ -2,7 +2,7 @@
 
 . "`dirname $0`/spec.sh"
 
-if [ -z "`vte-2.91 --version | grep "0\.67\.0.*\+SIXEL"`" ]; then
+if [ -z "`vte-2.91 --version | grep "\+SIXEL"`" ]; then
     echo -n "Wrong VTE version and options: "
     vte-2.91 --version
     exit 1;
