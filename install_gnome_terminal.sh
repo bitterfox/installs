5b85a813b435d07116504c450b76ae08237cb636
yoshida.shinya <yoshida.shinya@linecorp.com>
2022-04-09 15:38:27 +0900

    Update install_gnome_terminal.sh

--- install_gnome_terminal.sh
+++ install_gnome_terminal.sh
@@ -2,8 +2,9 @@
 
 . `dirname $0`/install_git_repos.sh
 
-gr_clone github.com bitterfox vte
+gnome-terminal --version
 
+gr_clone github.com bitterfox vte
 cd `gr root`/github.com/bitterfox/vte
 
 sudo apt install meson ninja-build gcc-10 g++-10 libvte-2.91-dev libsystemd-dev libgirepository1.0-dev valac libsixel-bin
@@ -20,7 +21,10 @@ cp /usr/local/lib/x86_64-linux-gnulibvte-2.91.so* /usr/lib/x86_64-linux-gnu/
 # Update org.gnome.Terminal.gschema.xml
 sudo cp /usr/share/glib-2.0/schemas/org.gnome.Terminal.gschema.xml /usr/share/glib-2.0/schemas/org.gnome.Terminal.gschema.xml.bak
 sudo sed -r '0,/range min="1.0"/s//range min="0.25"/' -i /usr/share/glib-2.0/schemas/org.gnome.Terminal.gschema.xml
+diff /usr/share/glib-2.0/schemas/org.gnome.Terminal.gschema.xml.bak /usr/share/glib-2.0/schemas/org.gnome.Terminal.gschema.xml
 glib-compile-schemas /usr/share/glib-2.0/schemas
+
+gnome-terminal --version
 
 # gr_clone github.com GNOME gnome-terminal
 
