fe3eae230e63d9207ea9e3710c61180c72a850b8
yoshida.shinya <yoshida.shinya@linecorp.com>
2022-04-09 17:08:37 +0900

    Fix libinput

--- install_libinput.sh
+++ install_libinput.sh
@@ -1,8 +1,27 @@
-# This does upgrade libinput actually for quirks
+#!/bin/bash
+# This does upgrade libinput actually for quirks for Ubuntu 18.04
+
+option=$1
 
 libinput --version
 
+libinput quirks validate
+if [ $? -eq 0 ]; then
+    echo "libinput quirks is supported"
+    if [ "$option" == "--force" ]; then
+        echo "Force update libinput"
+    else
+        echo "Skip libinput update"
+        exit 0
+    fi
+fi
+
 sudo add-apt-repository ppa:juliank/libinput-updates
+if [ $? -ne 0 ]; then
+    echo "Abort update libinput"
+    exit 0
+fi
+
 sudo apt-get update
 sudo apt "$APT_YES" upgrade
 
