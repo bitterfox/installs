92db8d4ab15b98607bdd66ceafe736c36c26ae6b
yoshida.shinya <yoshida.shinya@linecorp.com>
2022-04-09 17:23:03 +0900

    Update centraldogma version

--- install_central_dogma.sh
+++ install_central_dogma.sh
@@ -5,10 +5,11 @@ unlink ~/bin/dogma_line
 
 mkdir -p ~/bin/
 
-wget -O /tmp/centraldogma-0.48.0.tgz https://github.com/line/centraldogma/releases/download/centraldogma-0.48.0/centraldogma-0.48.0.tgz
-tar xfvm /tmp/centraldogma-0.48.0.tgz -C ~/bin/
+wget -O /tmp/centraldogma-0.55.1.tgz https://github.com/line/centraldogma/releases/download/centraldogma-0.55.1/centraldogma-0.55.1.tgz
 
-ln -s ~/bin/centraldogma-0.48.0/bin/native/dogma.linux-amd64 ~/bin/dogma
+tar xfvm /tmp/centraldogma-0.55.1.tgz -C ~/bin/
+
+ln -s ~/bin/centraldogma-0.55.1/bin/native/dogma.linux-amd64 ~/bin/dogma
 
 INSTALL_DIR=$(cd $(dirname $0); pwd)
 ln -s "$INSTALL_DIR/bin/dogma_line" ~/bin/dogma_line
