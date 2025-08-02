a9f7982841659a1e0acda72b20df72cd46ea72bb
yoshida.shinya <yoshida.shinya@linecorp.com>
2025-08-03 01:30:40 +0900

    za9c-r58 Install go 1.24.5

--- install_go.sh
+++ install_go.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-wget -O /tmp/go.tar.gz https://go.dev/dl/go1.18.1.linux-amd64.tar.gz
+wget -O /tmp/go.tar.gz https://go.dev/dl/go1.24.5.linux-amd64.tar.gz
 
 tar xfvm /tmp/go.tar.gz -C ~/bin/
 
-mv ~/bin/go ~/bin/go1.18.1
-ln -s ~/bin/go1.18.1/bin/go ~/bin/go
+mv ~/bin/go ~/bin/go1.24.5
+ln -s ~/bin/go1.24.5/bin/go ~/bin/go
