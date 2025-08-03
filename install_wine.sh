ab17d3640e265bf4581953898850bf51de583d92
yoshida.shinya <yoshida.shinya@linecorp.com>
2025-08-03 15:36:30 +0900

    za9c-r58 Update installing wine

--- install_wine.sh
+++ install_wine.sh
@@ -1,32 +1,34 @@
 #!/bin/sh
 
+. /etc/os-release
+
+# https://gitlab.winehq.org/wine/wine/-/wikis/Debian-Ubuntu
 # https://www.kkaneko.jp/tools/linuxtoolchain/wine.html
 # https://marmooo.blogspot.com/2020/01/apt.html
 
-wget -O /tmp/winehq.key -nc https://dl.winehq.org/wine-builds/winehq.key
-sudo apt-key add /tmp/winehq.key
-sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
-# sudo apt-add-repository https://dl.winehq.org/wine-builds/ubuntu/
+sudo dpkg --add-architecture i386
+
+sudo mkdir -pm755 /etc/apt/keyrings
+wget -O - https://dl.winehq.org/wine-builds/winehq.key | sudo gpg --dearmor -o /etc/apt/keyrings/winehq-archive.key -
+
+sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/noble/winehq-${VERSION_CODENAME}.sources
 
 sudo apt update
 
 #sudo dpkg --add-architecture i386
 #sudo apt install --install-recommends winehq-stable
 
-. /etc/os-release
-
 if [ "$VERSION_ID" = "18.04" ]; then
+    # libfaudio for 18.04 is not found now
     wget -O /tmp/libfaudio0_amd64.deb https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04/amd64/libfaudio0_19.07-0~bionic_amd64.deb
     wget -O /tmp/libfaudio0_i386.deb https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/xUbuntu_18.04/i386/libfaudio0_19.07-0~bionic_i386.deb
 
     sudo dpkg -i /tmp/libfaudio0_amd64.deb
     sudo dpkg -i /tmp/libfaudio0_i386.deb
-fi
 
-sudo apt install "$APT_YES" libc-bin
-sudo apt --fix-broken install "$APT_YES"
+    sudo apt install "$APT_YES" libc-bin
+    sudo apt --fix-broken install "$APT_YES"
 
-if [ "$VERSION_ID" = "18.04" ]; then
     sudo dpkg -i /tmp/libfaudio0_i386.deb
 fi
 
