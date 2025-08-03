733197f71f9759fe8dff1166e6e44e1c55480998
yoshida.shinya <yoshida.shinya@linecorp.com>
2025-08-03 10:50:52 +0900

    za9c-r58 Fix installing xkeysnail

--- install_xkeysnail.sh
+++ install_xkeysnail.sh
@@ -1,7 +1,8 @@
 
-sudo apt install "$APT_YES" python3-pip
-# xkeysnail v0.3.0 has an issue with key combination like C-x+C-x
-sudo pip3 install xkeysnail==0.2.0
+sudo apt install "$APT_YES" python3-pip pipx
+#xkeysnail v0.3.0 has an issue with key combination like C-x+C-x
+# compatibility issue with evdev 1.9.0: https://github.com/mooz/xkeysnail/issues/179
+sudo pip3 install xkeysnail==0.2.0 evdev==1.8.0 --break-system-packages
 
 sudo cp sudoers_xkeysnail /etc/sudoers.d
 sudo chown root:root /etc/sudoers.d/sudoers_xkeysnail
