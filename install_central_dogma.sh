e04b3f62f4cc18ce3d62f1584711fb4ed8a98dbc
yoshida.shinya <yoshida.shinya@linecorp.com>
2025-10-12 21:54:30 +0900

    mbp-m4pro-vmware More arm packages

--- install_central_dogma.sh
+++ install_central_dogma.sh
@@ -5,11 +5,11 @@ unlink ~/bin/dogma_line
 
 mkdir -p ~/bin/
 
-wget -O /tmp/centraldogma-0.55.1.tgz https://github.com/line/centraldogma/releases/download/centraldogma-0.55.1/centraldogma-0.55.1.tgz
+wget -O /tmp/centraldogma-0.77.4.tgz https://github.com/line/centraldogma/releases/download/centraldogma-0.77.4/centraldogma-0.77.4.tgz
 
-tar xfvm /tmp/centraldogma-0.55.1.tgz -C ~/bin/
+tar xfvm /tmp/centraldogma-0.77.4.tgz -C ~/bin/
 
-ln -s ~/bin/centraldogma-0.55.1/bin/native/dogma.linux-amd64 ~/bin/dogma
+ln -s ~/bin/centraldogma-0.77.4/bin/native/dogma.linux_arm64 ~/bin/dogma
 
 INSTALL_DIR=$(cd $(dirname $0); pwd)
 ln -s "$INSTALL_DIR/bin/dogma_line" ~/bin/dogma_line
